ba5413eeefc9e28fcff0758896bd1e3034cdc86b
Lubos Vozdecky <lubos.vozdecky@cern.ch>
2020-09-18 18:25:04 +0100

    created script for finding pipes

---
+++ findPipe.py
@@ -0,0 +1,7 @@
+import ROOT
+
+from frameanal import FrameAnalysis
+
+
+analysis = FrameAnalysis("ThermalImpedanceQA/test_upper.root", "config_CSV_with_0.92_emmy_upper", "ThermalImpedanceQA/output", False, False)
+
